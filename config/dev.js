f641b0de958b5d061682fee85531199e84b81371
swetttirexkiller <piotrjankiewicz02@gmail.com>
2018-05-04 17:29:36 +0200

    aws config

--- config/dev.js
+++ config/dev.js
@@ -1,8 +1,12 @@
+const {accessKeyId, secretAccessKey} = require('../secret');
+
 module.exports = {
     googleClientID:
         '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
     googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
     mongoURI: 'mongodb://localhost/AdvancedNode',
     cookieKey: '123123123',
-    redisUrl: 'redis://127.0.01:6379'
+    redisUrl: 'redis://127.0.01:6379',
+    accessKeyId,
+    secretAccessKey
 };
---
+++ secret.js
@@ -0,0 +1,4 @@
+module.exports = {
+    accessKeyId: 'AKIAIKE2YAKSQBXT7FEA',
+    secretAccessKey: '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'
+};
